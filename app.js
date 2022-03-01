463724afab90a7a6fb7537b22bc14616e7b73bd3
timDeHof <this.is.tim.dehof@gmail.com>
2022-03-01 10:23:07 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+// step 1
+
+// * Here is our game object
+
+
+// * write functions to manipulate gameState
+
+// * Put Stuff of the screen
+
+// * reference to our board
+
+// * Make them work together
+
+
+                    
